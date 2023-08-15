ed3eb0f1355fb977d93600db7d3e3a4c0cff5373
Steven Castro <castrostevenyc@gmail.com>
2023-08-15 19:42:56 -0400

    auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { withAuth } from "next-auth/middleware";
-export default withAuth(function middleware() {}, {
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    newUser: "",
-    error: "",
-    signIn: "/login",
-    signOut: "",
-    verifyRequest: "",
-  },
-});
+export { default } from "next-auth/middleware";
+// export default withAuth(function middleware() {}, {
+//   secret: process.env.NEXTAUTH_SECRET,
+//   pages: {
+//     newUser: "",
+//     error: "",
+//     signIn: "/login",
+//     signOut: "",
+//     verifyRequest: "",
+//   },
+// });
 export const config = { matcher: "/dashboard/:path*" };
