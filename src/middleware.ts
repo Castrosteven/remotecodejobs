12ce715450ba599f0bd52d3e31c6f2e2497e7456
Steven Castro <castrostevenyc@gmail.com>
2023-08-15 19:46:35 -0400

    f;

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-export { default } from "next-auth/middleware";
-// export default withAuth(function middleware() {}, {
-//   secret: process.env.NEXTAUTH_SECRET,
-//   pages: {
-//     newUser: "",
-//     error: "",
-//     signIn: "/login",
-//     signOut: "",
-//     verifyRequest: "",
-//   },
-// });
+import { withAuth } from "next-auth/middleware";
+export default withAuth(function middleware() {}, {
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    newUser: "",
+    error: "",
+    signIn: "/login",
+    signOut: "",
+    verifyRequest: "",
+  },
+});
 export const config = { matcher: "/dashboard/:path*" };
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,6 @@ import prisma from "@/utils/prisma";
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-  session: { strategy: "jwt" },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
